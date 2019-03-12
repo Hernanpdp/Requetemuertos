587d67c2d06491316d125c5b2a16d8d73d2980cc
Hernán <48439102+Hernanpdp@users.noreply.github.com>
2019-03-12 11:55:40 +0100

    Particle system ci

---
+++ script/particledeath.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particledeath : MonoBehaviour
+{
+    float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Rotate(90, 0, 0);
+        GetComponent<SphereCollider>().radius = 1;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "enemy")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        GetComponent<SphereCollider>().radius = timer*80;
+        if (timer > 0.5f)
+        {
+            Destroy(gameObject);
+            timer = 0;
+        }
+    }
+}
